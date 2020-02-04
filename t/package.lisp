880637a5cb63dbb3a894e2291b55de222fc10bc9
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2020-02-03 22:27:19 -0700

    Cleanup unit tests.

--- t/package.lisp
+++ t/package.lisp
@@ -25,40 +25,85 @@
 (def-suite :utm)
 (in-suite :utm)
 
+(defclass utm-test-case ()
+  ((latitude :initarg :latitude)
+   (longitude :initarg :longitude)
+   (ellipsoid :initarg :ellipsoid)
+   (zone :initarg :zone)
+   (easting :initarg :easting)
+   (northing :initarg :northing)))
+
+(defmethod print-object ((object utm-test-case) stream)
+  (with-slots (latitude longitude ellipsoid zone easting northing) object
+    (format stream "{ latitude: ~a, longitude: ~a, ellipsoid: ~a, zone: ~a easting: ~a, northing: ~a }"
+            latitude longitude ellipsoid zone easting northing)))
+
 (defparameter *utm-tolerance* 0.002)
-(defparameter *test-coords* '(
-                              #(39.9835310406d0 -105.2536113560d0 "NAD83" 13 478346.59856d0 4425960.12177d0)
-                              #(46.8392935284d0 -115.3348159790d0 "NAD83" 11 626974.542d0 5188651.724d0)
-                              #(48.2338208531d0 19.3716430664d0 "GRS80" 34 379083.606d0 5343570.714d0)
-                              #(59.8723979923d0 105.1062011719d0 "GRS80" 48 505946.450d0 6637205.350d0)
-                              #(-29.3438753994d0 130.8032226563d0 "GRS80" 52 675064.430d0 6752564.671d0)))
+
+(defparameter *test-coords*
+  (list (make-instance 'utm-test-case
+                       :latitude 39.9835310406d0
+                       :longitude -105.2536113560d0
+                       :ellipsoid "NAD83"
+                       :zone 13
+                       :easting 478346.59856d0
+                       :northing 4425960.12177d0)
+        (make-instance 'utm-test-case
+                       :latitude 46.8392935284d0
+                       :longitude -115.3348159790d0
+                       :ellipsoid "NAD83"
+                       :zone 11
+                       :easting 626974.542d0
+                       :northing 5188651.724d0)
+        (make-instance 'utm-test-case
+                       :latitude 48.2338208531d0
+                       :longitude 19.3716430664d0
+                       :ellipsoid "GRS80"
+                       :zone 34
+                       :easting 379083.606d0
+                       :northing 5343570.714d0)
+        (make-instance 'utm-test-case
+                       :latitude 59.8723979923d0
+                       :longitude 105.1062011719d0
+                       :ellipsoid "GRS80"
+                       :zone 48
+                       :easting 505946.450d0
+                       :northing 6637205.350d0)
+        (make-instance 'utm-test-case
+                       :latitude -29.3438753994d0
+                       :longitude 130.8032226563d0
+                       :ellipsoid "GRS80"
+                       :zone 52
+                       :easting 675064.430d0
+                       :northing 6752564.671d0)))
+
+(defun utm-near (a b)
+  (< (abs (- a b)) *utm-tolerance*))
+
 (test lat-lon-to-utm
   (dolist (coord *test-coords*)
-    (let* ((lat (aref coord 0))
-           (lon (aref coord 1))
-           (ellipsoid (aref coord 2))
-           (zone (aref coord 3))
-           (easting (aref coord 4))
-           (northing (aref coord 5))
-           (translated (lat-lon-to-utm lat lon :ellipsoid ellipsoid))
-           (teasting (car translated))
-           (tnorthing (cadr translated))
-           (tzone (caddr translated)))
-      (is-true (< (abs (- teasting easting)) *utm-tolerance*))
-      (is-true (< (abs (- tnorthing northing)) *utm-tolerance*))
-      (is-true (= zone tzone)))))
+    (with-slots (latitude longitude ellipsoid zone easting northing) coord
+      (let* ((translated (lat-lon-to-utm latitude longitude :ellipsoid ellipsoid))
+             (translated-easting (car translated))
+             (translated-northing (cadr translated))
+             (translated-zone (caddr translated)))
+        (is-true (utm-near translated-easting easting)
+                 "Could not convert ~a to UTM got easting ~a" coord translated-easting)
+        (is-true (utm-near translated-northing northing)
+                 "Could not convert ~a to UTM got northing ~a" coord translated-northing)
+        (is-true (= zone translated-zone)
+                 "Could not convert ~a to UTM got zone ~a" coord translated-zone)))))
 
 
 (test utm-to-lat-lon
   (dolist (coord *test-coords*)
-    (let* ((lat (aref coord 0))
-           (lon (aref coord 1))
-           (ellipsoid (aref coord 2))
-           (zone (aref coord 3))
-           (easting (aref coord 4))
-           (northing (aref coord 5))
-           (translated (utm-to-lat-lon easting northing zone :ellipsoid ellipsoid))
-           (tlat (car translated))
-           (tlon (cadr translated)))
-      (is-true (< (abs (- tlat lat)) *utm-tolerance*))
-      (is-true (< (abs (- tlon lon)) *utm-tolerance*)))))
+    (with-slots (latitude longitude ellipsoid zone easting northing) coord
+      (let* ((translated (utm-to-lat-lon easting northing zone :ellipsoid ellipsoid))
+             (translated-latitude (car translated))
+             (translated-longitude (cadr translated)))
+        (is-true (utm-near translated-latitude latitude)
+                 "Could not convert ~a to latitude/longitude... got latitude ~a"
+                 coord translated-latitude)
+        (is-true (utm-near translated-longitude longitude)
+                 "Could not convert ~a to latitude/longitude... got longitude ~a"
+                 coord translated-longitude)))))
